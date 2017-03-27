402ccb420faf0803cd2735870b92791f4b5425d0
Danny Ogen <dannyogen@Dannys-MacBook-Pro.local>
2017-03-27 10:59:52 +0300

    Hello world commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Simon Ng on 27/6/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
